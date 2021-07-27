2fd4308c6687a36c79807494d9cd99140b203d30
fatimazariwala <82675386+fatimazariwala@users.noreply.github.com>
2021-07-27 11:16:53 +0530

    Update lcm.c

--- lcm.c
+++ lcm.c
@@ -2,11 +2,9 @@
 int lcm(int a,int b);
   int main()
   {
-      int num2,num1;
-      printf("Enter first number: ",num2);
-      scanf("%d",&num2);
-      printf("\nEnter second number: ",num1);
-      scanf("%d",&num1);
+       int num2=12,num1=30;
+      printf("Input First number : %d",num2);
+      printf("\nInput Second number : %d",num1);
       printf("\nLCM : %d",lcm(num2,num1));
   }
  int lcm(int a ,int b)
